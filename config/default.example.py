55919e518d317ce2ffa5f63bc2c1c91b34bbb864
wyde wu <wyde@csie.ntu.edu.tw>
2017-09-17 19:43:07 +0800

    initial commit

---
+++ config/default.example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'user': 'dbuser',
+    'passwd': 'dbuser password',
+    'db': 'weather_report',
+    'charset': 'utf8'
+}
+
+MYSQL_ROOT_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'passwd': 'root password',
+    'charset': 'utf8'
+}
+
+HOST_CONFIG = {
+    'debug': True,
+    'dev': 'easy-data-processing-wyde.c9users.io'
+}
+
--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -58,9 +58,13 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    #'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 500
+    'PAGE_SIZE': 500,
+    #'UNICODE_JSON': False,
 }
 
 ROOT_URLCONF = 'djangorest.urls'
